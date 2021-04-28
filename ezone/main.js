ebac23b8afcc83bc8df5d66d68e0975dd2470331
Mateusz_Walowski <mat.walowski@gmail.com>
2021-04-28 17:37:08 +0200

    forms&databses done

--- ezone/main.js
+++ ezone/main.js
@@ -18,221 +18,272 @@ document.querySelector("#previous").addEventListener("click", previouspage);
 
 function nextpage() {
   if (document.querySelector("#page1").classList.contains("activepage")) {
-    // This hides first page
-    var pagetohide = document.querySelector("#page1");
-    pagetohide.animate(
-      [
-        {
-          opacity: "1",
-        },
-        {
-          opacity: "0",
-        },
-      ],
-      1000
-    );
-    setTimeout(function () {
-      pagetohide.style.display = "none";
-    }, 450);
-    pagetohide.classList.remove("activepage");
-    document.querySelector("#page2").classList.add("activepage");
+    if (
+      document.querySelector("#password").value == "" ||
+      document.querySelector("#fullname").value == "" ||
+      document.querySelector("#email").value == "" ||
+      document.querySelector("#dateofbirth").value == "" ||
+      document.querySelector("#gamertag").value == ""
+    ) {
+      document.querySelector("#fillthefieldspage1").textContent = "";
+      document.querySelector("#fillthefieldspage1").textContent =
+        document.querySelector("#fillthefieldspage1").textContent +
+        "Fill all the fields before continuing";
+    } else if (
+      document.querySelector("#password").value !==
+      document.querySelector("#repeatpassword").value
+    ) {
+      document.querySelector("#fillthefieldspage1").textContent = "";
 
-    // This shows  the second page
-    document.querySelector("#signup").animate(
-      [
-        {
-          opacity: "0",
-        },
-        {
-          opacity: "1",
-        },
-      ],
-      3000
-    );
-    document.querySelector("#guide > h2").animate(
-      [
-        {
-          opacity: "0",
-        },
-        {
-          opacity: "1",
-        },
-      ],
-      3000
-    );
+      document.querySelector("#fillthefieldspage1").innerHTML =
+        document.querySelector("#fillthefieldspage1").textContent +
+        "<br>" +
+        " Passwords don't match";
+    } else if (!document.querySelector("#email").value.includes("@")) {
+      document.querySelector("#fillthefieldspage1").textContent = "";
 
-    document.querySelector("#page1").animate(
-      [
-        {
-          opacity: "0",
-        },
-        {
-          opacity: "1",
-        },
-      ],
-      2000
-    );
+      document.querySelector("#fillthefieldspage1").innerHTML =
+        document.querySelector("#fillthefieldspage1").textContent +
+        "<br>" +
+        "Write a correct email format";
+    } else {
+      // This hides first page
+      var pagetohide = document.querySelector("#page1");
+      pagetohide.animate(
+        [
+          {
+            opacity: "1",
+          },
+          {
+            opacity: "0",
+          },
+        ],
+        1000
+      );
+      setTimeout(function () {
+        pagetohide.style.display = "none";
+      }, 450);
+      pagetohide.classList.remove("activepage");
+      document.querySelector("#page2").classList.add("activepage");
 
-    setTimeout(function () {
-      document.querySelector("#page2").style.display = "block";
-    }, 500);
-    // Changes the text above and next to the form
-    document.querySelector("#signup").textContent =
-      "Text to show above form at page2";
-    document.querySelector("#guide > h2").textContent =
-      "text on the right on page2";
-    // changes the color of dots and sidebar
-    document.querySelector("#dot2").style.background = "var(--purple-color)";
-    document.querySelector("#sidebar > li:nth-child(3)").style.background =
-      "#E3E3E5";
-    document.querySelector("#sidebar > li.number.on").style.background =
-      "#FFB595";
+      // This shows  the second page
+      document.querySelector("#signup").animate(
+        [
+          {
+            opacity: "0",
+          },
+          {
+            opacity: "1",
+          },
+        ],
+        3000
+      );
+      document.querySelector("#guide > h2").animate(
+        [
+          {
+            opacity: "0",
+          },
+          {
+            opacity: "1",
+          },
+        ],
+        3000
+      );
+
+      document.querySelector("#page1").animate(
+        [
+          {
+            opacity: "0",
+          },
+          {
+            opacity: "1",
+          },
+        ],
+        2000
+      );
+
+      setTimeout(function () {
+        document.querySelector("#page2").style.display = "block";
+      }, 500);
+      // Changes the text above and next to the form
+      document.querySelector("#signup").textContent =
+        "Text to show above form at page2";
+      document.querySelector("#guide > h2").textContent =
+        "text on the right on page2";
+      // changes the color of dots and sidebar
+      document.querySelector("#dot2").style.background = "var(--purple-color)";
+      document.querySelector("#sidebar > li:nth-child(3)").style.background =
+        "#E3E3E5";
+      document.querySelector("#sidebar > li.number.on").style.background =
+        "#FFB595";
+    }
   } else if (
     document.querySelector("#page2").classList.contains("activepage")
   ) {
-    // This hides second page
-    var pagetohide = document.querySelector("#page2");
-    pagetohide.animate(
-      [
-        {
-          opacity: "1",
-        },
-        {
-          opacity: "0",
-        },
-      ],
-      1000
-    );
-    setTimeout(function () {
-      pagetohide.style.display = "none";
-    }, 450);
-    pagetohide.classList.remove("activepage");
-    document.querySelector("#page3").classList.add("activepage");
+    if (document.getElementById("genre").selectedOptions.length == 0) {
+      document.querySelector("#fillthefieldspage2").textContent =
+        "Select at least one option";
+    } else {
+      console.log(document.getElementById("genre").selectedOptions.length);
+      // This hides second page
+      var pagetohide = document.querySelector("#page2");
+      pagetohide.animate(
+        [
+          {
+            opacity: "1",
+          },
+          {
+            opacity: "0",
+          },
+        ],
+        1000
+      );
+      setTimeout(function () {
+        pagetohide.style.display = "none";
+      }, 450);
+      pagetohide.classList.remove("activepage");
+      document.querySelector("#page3").classList.add("activepage");
 
-    // This shows  the third page
-    document.querySelector("#signup").animate(
-      [
-        {
-          opacity: "0",
-        },
-        {
-          opacity: "1",
-        },
-      ],
-      3000
-    );
-    document.querySelector("#guide > h2").animate(
-      [
-        {
-          opacity: "0",
-        },
-        {
-          opacity: "1",
-        },
-      ],
-      3000
-    );
+      // This shows  the third page
+      document.querySelector("#signup").animate(
+        [
+          {
+            opacity: "0",
+          },
+          {
+            opacity: "1",
+          },
+        ],
+        3000
+      );
+      document.querySelector("#guide > h2").animate(
+        [
+          {
+            opacity: "0",
+          },
+          {
+            opacity: "1",
+          },
+        ],
+        3000
+      );
 
-    document.querySelector("#page3").animate(
-      [
-        {
-          opacity: "0",
-        },
-        {
-          opacity: "1",
-        },
-      ],
-      2000
-    );
+      document.querySelector("#page3").animate(
+        [
+          {
+            opacity: "0",
+          },
+          {
+            opacity: "1",
+          },
+        ],
+        2000
+      );
 
-    setTimeout(function () {
-      document.querySelector("#page3").style.display = "block";
-    }, 500);
-    // Changes the text above and next to the form
-    document.querySelector("#signup").textContent =
-      "Text to show above form at page3";
-    document.querySelector("#guide > h2").textContent =
-      "text on the right on page3";
-    // changes the color of dots and sidebar
-    document.querySelector("#dot3").style.background = "var(--purple-color)";
-    document.querySelector("#sidebar > li:nth-child(3)").style.background =
-      "#E3E3E5";
-    document.querySelector("#sidebar > li.number.on").style.background =
-      "#E3E3E5";
-    document.querySelector("#sidebar > li:nth-child(1)").style.background =
-      "rgb(255, 181, 149)";
+      setTimeout(function () {
+        document.querySelector("#page3").style.display = "block";
+      }, 500);
+      document.querySelector("#next").addEventListener("click", postdata);
+      document.querySelector("#next").textContent = "Submit";
+
+      // Changes the text above and next to the form
+      document.querySelector("#signup").textContent =
+        "Text to show above form at page3";
+      document.querySelector("#guide > h2").textContent =
+        "text on the right on page3";
+      // changes the color of dots and sidebar
+      document.querySelector("#dot3").style.background = "var(--purple-color)";
+      document.querySelector("#sidebar > li:nth-child(3)").style.background =
+        "#E3E3E5";
+      document.querySelector("#sidebar > li.number.on").style.background =
+        "#E3E3E5";
+      document.querySelector("#sidebar > li:nth-child(1)").style.background =
+        "rgb(255, 181, 149)";
+    }
   } else if (
     document.querySelector("#page3").classList.contains("activepage")
   ) {
-    // This hides third page
-    var pagetohide = document.querySelector("#page3");
-    pagetohide.animate(
-      [
-        {
-          opacity: "1",
-        },
-        {
-          opacity: "0",
-        },
-      ],
-      1000
-    );
-    setTimeout(function () {
-      pagetohide.style.display = "none";
-    }, 450);
-    pagetohide.classList.remove("activepage");
-    document.querySelector("#thanks").classList.add("activepage");
+    if (document.getElementById("areas").selectedOptions.length == 0) {
+      document.querySelector("#fillthefieldspage3").textContent =
+        "Select at least one option";
+    } else {
+      // This hides third page
+      var pagetohide = document.querySelector("#page3");
+      pagetohide.animate(
+        [
+          {
+            opacity: "1",
+          },
+          {
+            opacity: "0",
+          },
+        ],
+        1000
+      );
+      setTimeout(function () {
+        pagetohide.style.display = "none";
+      }, 450);
+      pagetohide.classList.remove("activepage");
+      document.querySelector("#thanks").classList.add("activepage");
 
-    // This shows  thank you pagge
-    document.querySelector("#signup").animate(
-      [
-        {
-          opacity: "0",
-        },
-        {
-          opacity: "1",
-        },
-      ],
-      3000
-    );
-    document.querySelector("#guide > h2").animate(
-      [
-        {
-          opacity: "0",
-        },
-        {
-          opacity: "1",
-        },
-      ],
-      3000
-    );
+      // This shows  thank you pagge
+      document.querySelector("#signup").animate(
+        [
+          {
+            opacity: "0",
+          },
+          {
+            opacity: "1",
+          },
+        ],
+        3000
+      );
+      document.querySelector("#guide > h2").animate(
+        [
+          {
+            opacity: "0",
+          },
+          {
+            opacity: "1",
+          },
+        ],
+        3000
+      );
 
-    document.querySelector("#thanks").animate(
-      [
-        {
-          opacity: "0",
-        },
-        {
-          opacity: "1",
-        },
-      ],
-      2000
-    );
+      document.querySelector("#thanks").animate(
+        [
+          {
+            opacity: "0",
+          },
+          {
+            opacity: "1",
+          },
+        ],
+        2000
+      );
 
-    setTimeout(function () {
-      document.querySelector("#thanks").style.display = "block";
-    }, 500);
-    // Changes the text above and next to the form
-    document.querySelector("#signup").textContent =
-      "Text to show above form at page4";
-    document.querySelector("#guide > h2").textContent =
-      "text on the right on page4";
+      setTimeout(function () {
+        document.querySelector("#thanks").style.display = "block";
+      }, 500);
+      document.querySelector("#next").style.display = "none";
+      document.querySelector("#previous").style.display = "none";
+      // Changes the text above and next to the form
+      document.querySelector("#signup").textContent =
+        "Text to show above form at page4";
+      document.querySelector("#guide > h2").textContent =
+        "text on the right on page4";
+    }
   }
 }
 
 function previouspage() {
   if (document.querySelector("#page2").classList.contains("activepage")) {
     var pagetohide = document.querySelector("#page2");
+    document.querySelector("#next").textContent = "next 🠖>";
+    document.querySelector("#fillthefieldspage1").textContent = "";
+    document.querySelector("#fillthefieldspage2").textContent = "";
+    document.querySelector("#fillthefieldspage3").textContent = "";
+
     pagetohide.animate(
       [
         {
@@ -301,6 +352,10 @@ function previouspage() {
   }
 
   if (document.querySelector("#page3").classList.contains("activepage")) {
+    document.querySelector("#next").textContent = "next 🠖>";
+    document.querySelector("#fillthefieldspage1").textContent = "";
+    document.querySelector("#fillthefieldspage2").textContent = "";
+    document.querySelector("#fillthefieldspage3").textContent = "";
     var pagetohide = document.querySelector("#page3");
     pagetohide.animate(
       [
@@ -372,32 +427,42 @@ function previouspage() {
   }
 }
 
-document.querySelector("#next").addEventListener("click", postdata);
-
 function postdata() {
   var url = "https://ezone-4491.restdb.io/rest/ezone";
-  var key = "32e6dd2cfb967e015898fb0d5313a3b2c3cb1";
+  var key = "60868efc28bf9b609975a6f1";
 
   var password = document.querySelector("#password").value;
-  var fullname = document.querySelector("#password").value;
-  var email = document.querySelector("#password").value;
-  var dateofbirth = document.querySelector("#password").value;
-  var gamertag = document.querySelector("#password").value;
+  var fullname = document.querySelector("#fullname").value;
+  var email = document.querySelector("#email").value;
+  var dateofbirth = document.querySelector("#dateofbirth").value;
+  var gamertag = document.querySelector("#gamertag").value;
+
+  var optionsgenre = document.getElementById("genre").selectedOptions;
+  var genres = Array.from(optionsgenre).map(({ value }) => value);
+  console.log(genres);
+
+  var optionskills = document.getElementById("areas").selectedOptions;
+  var skills = Array.from(optionskills).map(({ value }) => value);
+  console.log(skills);
 
   const data = {
     password: password,
     fullname: fullname,
     email: email,
     gamertag: gamertag,
-    date: dateofbirth,
+    dateofbirth: dateofbirth,
+    genres: genres,
+    skills: skills,
   };
 
   const postData = JSON.stringify(data);
   fetch(url, {
     method: "post",
     headers: {
+      // "Access-Control-Allow-Origin": "*",
+      // "Access-Control-Allow-Headers": "*",
       "Content-Type": "application/json; charset=utf-8",
-      "x-apikey": "32e6dd2cfb967e015898fb0d5313a3b2c3cb1",
+      "x-apikey": "60868efc28bf9b609975a6f1",
       "cache-control": "no-cache",
     },
     body: postData,
